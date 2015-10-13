ee4f393ac499bbf283e31ac1089d5342e9f84a38
Tom Messerschmidt <tom.messerschmidt@googlemail.com>
2015-10-13 15:08:06 +0200

    Version 0.02
    Now it prints Errors if the Config file is invalid.

--- src/main.php
+++ src/main.php
@@ -1,6 +1,6 @@
 <?php
 
-	$version = 0.01;
+	$version = 0.02;
 
 	print (":: Jetspace Hangar Version " . $version . " started!\n");
 
@@ -13,26 +13,47 @@
 	print(":: Configuration\n");
 	if(isset($ini['Settings']['isCritical']))
 	{
-		$isCritical = $ini['Settings']['isCritical'];
-		if($isCritical == 1)
+		if($ini['Settings']['isCritical'] == "true" || $ini['Settings']['isCritical'] == "false")
 		{
-			$isCritical = true;
+			$isCritical = $ini['Settings']['isCritical'];
+			if($isCritical == 1)
+			{
+				$isCritical = true;
+			}
+			else
+			{
+				$isCritical = false;
+			}
+			echo $isCritical ? "\tisCritical set to true\n" : "\tisCritical set to false\n";
 		}
 		else
 		{
-			$isCritical = false;
+			echo ":: [WARNING] isCritical has to be \"true\" or \"false\"\n";
 		}
-		echo $isCritical ? "\tisCritical set to true\n" : "\tisCritical set to false\n";
 	}
 
 	if(isset($ini['Settings']['success']))
 	{
-		$success = $ini['Settings']['success'];
+		if(is_numeric($ini['Settings']['success']) == TRUE)
+		{
+			$success = $ini['Settings']['success'];
+		}
+		else
+		{
+			$success = ":: [WARNING] Return Value must be a positive number!";
+		}
 	}
 
 	if(isset($ini['Settings']['failure']))
 	{
-		$failure = $ini['Settings']['failure'];
+		if(is_numeric($ini['Settings']['failure']) == TRUE)
+		{
+			$failure = $ini['Settings']['failure'];
+		}
+		else
+		{
+			$failure = ":: [WARNING] Return Value must be a positive number!";
+		}
 	}
 
 	print ":: Return Values:\n";
